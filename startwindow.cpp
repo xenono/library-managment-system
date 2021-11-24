35900f9568bc515ca839c4483c4a458dd7f241ef
xenono <aurbanczyk0@gmail.com>
2021-11-24 13:28:45 +0000

    Add start view

---
+++ startwindow.cpp
@@ -0,0 +1,29 @@
+#include "startwindow.h"
+#include "ui_startwindow.h"
+
+StartWindow::StartWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::StartWindow)
+{
+    ui->setupUi(this);
+}
+
+StartWindow::~StartWindow()
+{
+    delete ui;
+}
+
+void StartWindow::on_pushButton_clicked()
+{
+    qInfo("Login");
+}
+
+void StartWindow::on_pushButton_2_clicked()
+{
+    qInfo("Sign up");
+}
+
+
+
+
+
